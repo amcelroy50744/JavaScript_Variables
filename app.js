64dff3ee5c6a63a302e562957ed3dc469309ff57
Andre McElroy jr <amcelroy50744@gmail.com>
2021-09-09 15:18:48 -0500

    Java Script

--- app.js
+++ app.js
@@ -4,7 +4,15 @@ console.log(
 );
 
 // Exercise 1
-
+const firstName = "Andre";
+let lastName = "McElroy";
+var myAge = 30;
 // Exercise 2
-
+let fullName = firstName + " " + lastName;
+console.log("fullName:", fullName);
 // Exercise 3
+let myStory;
+let city = "Madison";
+let pastTime = "Basketball, reading, coding, video games";
+myStory = "My name is " + fullName + ". "+ "I live in " + city + ". " + "I enjoy " + pastTime + ".";
+console.log(myStory);
